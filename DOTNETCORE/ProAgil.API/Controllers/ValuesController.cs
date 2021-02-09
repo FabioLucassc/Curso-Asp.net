a41561ebefb2cb1994dc53d686a0bf8eb448269f
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-09 17:58:46 -0300

    test

--- DOTNETCORE/ProAgil.API/Controllers/ValuesController.cs
+++ DOTNETCORE/ProAgil.API/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.API.Model;
 // using ProAgil.API.Model;
 
 namespace ProAgil.API.Controllers
